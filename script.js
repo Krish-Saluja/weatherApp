9b2572e487a73d96d6827eb0f03dcb689cce10c8
Krish Saluja <krishsaluja297@gmail.com>
2024-02-18 10:11:25 +0530

    Final app that shows temperature based on the location entered

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-const apiKey = "api key";
+const apiKey = "b2845053de1735fd32a1aa6279dbde9e";
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+document.getElementById('btn').addEventListener("click",() =>{
+    const cityInput = document.querySelector('#srch input');
+const city = cityInput ? cityInput.value : '';
+
+    if (city==="")
+    return;
+
+    fetch(`${apiUrl}?q=${city}&appid=${apiKey}`)
+    .then(response => response.json())
+        .then(data => {
+            // Handle the weather data here
+            const temperatureElement = document.getElementById('temperature');
+            const temperatureCelsius = Math.round(data.main.temp - 273.15);
+            temperatureElement.textContent = `Temperature: ${temperatureCelsius} °C`;
+        
+            console.log(data);
+        })
+        .catch(error => {
+            // Handle errors here
+            console.error('Error fetching weather data:', error);
+        });
+});
+
+
